685b53485d4547a3256509438ea828ae2b031c58
Conrad Kurth <conradkurth@gmail.com>
2023-11-27 10:49:24 -0800

    feat: adding in before send transaction option

--- service/option.go
+++ service/option.go
@@ -9,6 +9,7 @@ type options struct {
 	traceSampleRate   float64
 	profileSameplRate float64
 	traceSampler      sentry.TracesSampler
+	beforeSend        func(event *sentry.Event, hint *sentry.EventHint) *sentry.Event
 }
 
 // WithGRPCService will enable to service to run with a temporal worker
@@ -47,5 +48,11 @@ func WithProfileSampleRate(r float64) func(*options) {
 func WithTraceSampler(t sentry.TracesSampler) func(o *options) {
 	return func(o *options) {
 		o.traceSampler = t
+	}
+}
+
+func WithBeforeSendTransaction(t func(event *sentry.Event, hint *sentry.EventHint) *sentry.Event) func(o *options) {
+	return func(o *options) {
+		o.beforeSend = t
 	}
 }
